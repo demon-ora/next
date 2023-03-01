59c336c83cabab190065ff0e0d83dce70c3e269d
demon-ora <softwareselling26@gmail.com>
2023-03-01 14:05:09 +0545

    added custom error page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const error = () => {
+  return (
+    <>
+      <h1>eroor page not found return to</h1>
+      <a href="/">home page</a>
+    </>
+  );
+};
+
+export default error;
