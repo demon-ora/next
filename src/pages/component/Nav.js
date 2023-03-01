50fa3416737e2b7d37214175d22ed763d18ab2f1
demon-ora <softwareselling26@gmail.com>
2023-03-01 14:37:18 +0545

    made seperate component for navbar

---
+++ src/pages/component/Nav.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../../styles/nav.module.css";
+
+const Nav = () => {
+  return (
+    <>
+      <nav className={styles.box}>
+        <Link href="/" className={styles.link}>
+          here
+        </Link>
+        <Link href="/home" className={styles.link}>
+          home
+        </Link>
+        <Link href="/blog/blog" className={styles.link}>
+          blog
+        </Link>
+        <Link href="/about" className={styles.link}>
+          about
+        </Link>
+        <Link href="/blog/pageno" className={styles.link}>
+          pageno
+        </Link>
+      </nav>
+    </>
+  );
+};
+
+export default Nav;
