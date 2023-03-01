2d0f577543c19528f95c95b6d63620533eb32532
demon-ora <softwareselling26@gmail.com>
2023-03-01 14:25:52 +0545

    redirect after 5 sec using router

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,14 @@
+import Router from "next/router";
 import React from "react";
+import { useEffect } from "react";
 
 const error = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      Router.push("/");
+    }, 2000);
+  }, []);
+
   return (
     <>
       <h1>eroor page not found return to</h1>
